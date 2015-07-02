acfca60097bd184622840ed8cea758957402d8ef
Ruslan Ostafiychuk <rostafiychuk@apache.org>
2015-07-02 19:36:38 +0300

    FALCON-1306 Custom window dimensions for UI tests. Contributed by Ruslan Ostafiychuk (cherry picked from commit 9a29925)

--- falcon-regression/merlin/src/main/java/org/apache/falcon/regression/testHelper/BaseUITestClass.java
+++ falcon-regression/merlin/src/main/java/org/apache/falcon/regression/testHelper/BaseUITestClass.java
@@ -19,6 +19,7 @@
 package org.apache.falcon.regression.testHelper;
 
 import org.apache.falcon.regression.core.enumsAndConstants.MerlinConstants;
+import org.apache.falcon.regression.core.util.Config;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
@@ -45,8 +46,16 @@ public class BaseUITestClass extends BaseTestClass{
 
         driver = new FirefoxDriver(profile);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setPosition(new Point(0, 0));
-        driver.manage().window().setSize(new Dimension(1280, 1024));
+
+        int width = Config.getInt("browser.window.width", 0);
+        int height = Config.getInt("browser.window.height", 0);
+
+        if (width * height == 0) {
+            driver.manage().window().maximize();
+        } else {
+            driver.manage().window().setPosition(new Point(0, 0));
+            driver.manage().window().setSize(new Dimension(width, height));
+        }
 
     }
 
